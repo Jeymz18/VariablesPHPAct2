9af96f153d571d1f5972b0462bb6f4200645061b
Jeymz18 <ayuntingjames12@gmail.com>
2025-08-13 01:20:04 +0000

    ACT2

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+$Name = "James Ayunting";
+$Age = "20";
+$School = "Global Reciprocal College";
+$Course = "BSIT";
+$Subject = "WebSys";
+$Students = true;
+
+echo "Name: " . $Name . "<br>";
+echo "Age: " . $Age . "<br>";
+echo "School: " . $School . "<br>";
+echo "Course: " . $Course . "<br>";
+echo "Subject: " . $Subject . "<br>";
